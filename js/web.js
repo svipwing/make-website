3e870f85e654cf14e284f867b16c2055eeaa337e
svipwing <3260130869@qq.com>
2023-08-12 10:33:32 +0800

    [update] 宣传页新增响应式设计

--- js/web.js
+++ js/web.js
@@ -8,4 +8,19 @@ $(document).ready(function () {
     $("#qq").click(function(){
         swal({'title':'提示','text':'QQ交流群的群号：135452025','icon':'success'});
     });
+
+    if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+        $("#nav-pc").hide();
+        $("#helpme").hide();
+        $("#aboutme").hide();
+        $("#startuse").hide();
+        $('.da').removeClass("da");
+
+        $.growl.notice({
+            title: "提示",
+            message:
+              "建议使用电脑端打开，移动端建议切换为横屏以获得更好的体验",
+            duration: 10000,
+        });
+    }
 });
