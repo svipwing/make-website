b0774a9a3b65ceeec49f8f186db0d8d2a3b2dd34
Fgaoxing <Fgaoxing0206@163.com>
2024-01-23 15:21:56 +0800

    [style] 格式化代码

--- README.md
+++ README.md
@@ -16,9 +16,11 @@ nodejs编写，采用ejs。开源许可证：GPL-3
 2. 配置nodejs环境，按需设置npm镜像源
 3. 编写`config.json`配置文件
 4. 运行构建指令，首次使用需手动执行`npm install`
+
 ```
 npm run build
 ```
+
 5. 将网站运行目录设为`public`
 
 #### 注意事项
--- index.js
+++ index.js
@@ -9,18 +9,18 @@ const outputDir = './public';
 data = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 
 function processTemplate(filePath) {
-    if (filePath.substring(filePath.lastIndexOf(".") + 1) == "html" || (filePath.substring(filePath.lastIndexOf(".") + 1) == "js" && ! ["blockly.min","highlight.min","icon","html2canvas"].includes(Path.parse(filePath).name))) {
-        console.log("正在处理文件 "+filePath);
+    if (filePath.substring(filePath.lastIndexOf(".") + 1) == "html" || (filePath.substring(filePath.lastIndexOf(".") + 1) == "js" && !["blockly.min", "highlight.min", "icon", "html2canvas"].includes(Path.parse(filePath).name))) {
+        console.log("正在处理文件 " + filePath);
 
         const templateContent = fs.readFileSync(filePath, 'utf8');
         const result = ejs.render(templateContent, data);
         const fileName = path.relative(path.resolve(templateDir), filePath);
         const outputPath = path.join(outputDir, path.dirname(fileName));
-        fs.mkdirSync(outputPath, { recursive: true });
+        fs.mkdirSync(outputPath, {recursive: true});
         const outputFilePath = path.join(outputDir, fileName);
         fs.writeFileSync(outputFilePath, result);
-    }else{
-        console.log("正在拷贝文件"+filePath);
+    } else {
+        console.log("正在拷贝文件" + filePath);
         fs.cp(filePath, path.join(outputDir, path.relative(path.resolve(templateDir), filePath)), (err) => {
             if (err) {
                 console.error(err);
@@ -45,9 +45,9 @@ function processDirectory(dir) {
 
 console.log("正在准备清空缓存");
 if (fs.existsSync(outputDir)) {
-    fs.rmSync(outputDir, { recursive: true });
+    fs.rmSync(outputDir, {recursive: true});
     console.log(`${outputDir} 目录已删除`);
-}else{
+} else {
     console.log(`${outputDir} 目录不存在`);
 }
 
