8d07947da7b460abf1a59e0d488933c877fb183c
svipwing <3260130869@qq.com>
2024-03-15 21:20:13 +0800

    [feat] 导出导入功能支持文件格式

--- js/main.js
+++ js/main.js
@@ -84,6 +84,29 @@ $(document).ready(function () {
         });
     });
 
+    document.getElementById('importFile').addEventListener('change', async (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const code = await file.text();
+            Blockly.serialization.workspaces.load(JSON.parse(code), workspace);
+            
+            swal({
+                title: "导入完毕",
+                text: "积木数据导入完毕，请前往工作区查看！",
+                icon: "info"
+            });
+        }
+    });
+
+    function downloadText(fileName, text) {
+        const url = window.URL || window.webkitURL || window;
+        const blob = new Blob([text]);
+        const saveLink = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
+        saveLink.href = url.createObjectURL(blob);
+        saveLink.download = fileName;
+        saveLink.click();
+    }
+
     $("#b").click(function () {
         var json = Blockly.serialization.workspaces.save(workspace);
         $("#json").val(JSON.stringify(json));
@@ -102,6 +125,8 @@ $(document).ready(function () {
             text: "积木数据导出成功！已复制到剪贴板！",
             icon: "success"
         });
+
+        downloadText('page1.json', JSON.stringify(json));
     });
 
     $("#qq").click(function () {
