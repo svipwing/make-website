8ed416f33389e0dc137a8d628a539523441bae46
技术云 <3260130869@qq.com>
2024-07-18 14:47:35 +0800

    [feat] 毛玻璃优化

--- js/icon.js
+++ js/icon.js
@@ -4,13 +4,15 @@ class CustomCategory extends Blockly.ToolboxCategory {
     }
 
     createIconDom_() {
-        // 这里新建了一个iconpark-icon元素，也可以自己修改为别的元素
+        if(this.toolboxItemDef_.categorystyle == "icon-1"){
+            const img = document.createElement("iconpark-icon");
+            img.name = "icon-1";
+        }
+
         const img = document.createElement("iconpark-icon");
-        // 这里的name属性获取的内容是toolbox定义的中的categorystyle，也可以是别的值/属性
         img.name = this.toolboxItemDef_.categorystyle;
-        // 设置图标大小
+
         img.size = "15";
-        // 最后返回的应是一个元素
         return img;
     }
 }
