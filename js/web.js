6a13e8dda66d9a2ea95cc04fac83e473a817a145
SodiumCode <3409473369@qq.com>
2024-08-25 16:30:25 +0800

    [feat] 新官网

--- js/web.js
+++ js/web.js
@@ -3,16 +3,9 @@ $(document).ready(function () {
         swal({'title': '加入QQ交流群', 'text': '群号：135452025'});
     });
 
-    // if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
-    //     $("#pc").hide();
-    //     $("#gitee_repo_pc").hide();
-    //     $('.double').removeClass("double");
-
-    //     $.growl.notice({
-    //         title: "提示",
-    //         message:
-    //             "建议使用电脑端打开，移动端建议切换为横屏以获得更好的体验",
-    //         duration: 10000,
-    //     });
-    // }
+    if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+        $('.intro').addClass("flex-col");
+        $('.group').addClass("grid-1");
+        $('body').css('padding', '16px 32px');
+    }
 });
